8a646641893ba8f03322a16cce5dfbda66d67fde
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-28 10:07:43 +0600

    validate time regular expresion

--- middleware/validateEventInput.js
+++ middleware/validateEventInput.js
@@ -1,4 +1,5 @@
 const AppError = require("../utils/AppError");
+const validateDate = require("../utils/validateDate");
 
 const validateEventInput = (res, req, next) => {
   const { title, date, starttime, endtime } = req.body;
@@ -9,5 +10,8 @@ const validateEventInput = (res, req, next) => {
     return next(
       new AppError(`${missingFields.join(", ")} are required fields.`, 400)
     );
+  }
+  if (!validateDate(date)) {
+    return next(new AppError("Please input valid date! ", 400));
   }
 };
---
+++ utils/validateTime.js
@@ -0,0 +1,17 @@
+function validateTime(timeStr) {
+  // Define a regular expression pattern for the 24-hour time format
+  const pattern = /^([01]\d|2[0-3]):[0-5]\d$/;
+
+  if (!pattern.test(timeStr)) {
+    return false;
+  }
+
+  // Split the time string into its hours and minutes components
+  const [hours, minutes] = timeStr.split(":").map(Number);
+
+  if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    return false;
+  }
+
+  return true;
+}
