500dbbbcbcfc24f895603eaa755a81b10ce8db18
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-28 10:57:28 +0600

    refactor: Improve input validation logic in validateEventInput

--- middleware/validateEventInput.js
+++ middleware/validateEventInput.js
@@ -23,11 +23,11 @@ const validateEventInput = (req, _res, next) => {
     return next(new AppError("Please input valid date! 2023-8-1", 400));
 
   // Validate the start time format using the validateTime function
-  if (validateTime(starttime))
+  if (!validateTime(starttime))
     return next(new AppError("Please input valid start Time! 23:59", 400));
 
   // Validate the end time format using the validateTime function
-  if (validateTime(endtime))
+  if (!validateTime(endtime))
     return next(new AppError("Please input valid end Time! 23:59", 400));
 
   // If all validations pass, move on to the next middleware
