86901aa9b6c1866727bb1d5024647780375ebc4c
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-29 17:51:24 +0600

    refactor: Update Event mongoose model schema
    Refactored the mongoose schema for the 'Event' collection. The changes include:
    - Added 'userId' field of type String, required for user identification.
    - Updated schema description to reflect the new changes.
    
    This refactor ensures better data organization and adheres to the requirement of storing
    user IDs alongside events for better association and queries.

--- models/eventModel.js
+++ models/eventModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema(
   {
+    userId: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String },
     date: { type: String, required: true },
